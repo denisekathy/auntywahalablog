4952b30209a2de6b84ce5df70160ae4299fa7b3a
Denise Onukwue <donukwue1@gmail.com>
2021-05-02 12:44:47 -0400

    add env file to remove issues

--- crud-helper.js
+++ crud-helper.js
@@ -1,12 +1,12 @@
-// Connect the the database
-require('dotenv').config();
-require('./config/database');
+// // Connect the the database
+// require('dotenv').config();
+// require('./config/database');
 
-// Require the Mongoose models
-const User = require('./models/user');
-// const Item = require('/models/item');
-// const Category = require('./models/category');
-// const Order = require('./models/order');
+// // Require the Mongoose models
+// const User = require('./models/user');
+// // const Item = require('/models/item');
+// // const Category = require('./models/category');
+// // const Order = require('./models/order');
 
-// Local variables will come in handy
-let u, i, c, o;
+// // Local variables will come in handy
+// let u, i, c, o;
