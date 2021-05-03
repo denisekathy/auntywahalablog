3f9cf4c42169b26c748f65381f74c23a2cd5ab3b
Denise Onukwue <donukwue1@gmail.com>
2021-05-03 17:56:43 -0400

    adds comment box

--- src/Components/CommentBox/CommentBox.jsx
+++ src/Components/CommentBox/CommentBox.jsx
@@ -3,8 +3,27 @@ import react from 'react'
 export default function CommentBox(){
 
     return(
-        <>
-<p>Comments!</p>
-            </>
+   
+   <> 
+   <h1>Leave a Comment</h1>
+<form>
+    <div>
+<textarea name="textarea" id="textarea" cols="50" rows="10" placeholder="Comment Here..."></textarea>
+</div>
+<div>
+<button type='submit' className='btn' >
+					Submit Comment
+				</button>
+</div>
+
+</form>
+       
+</>
+  
+
+        
+
+
+         
     )
 }
