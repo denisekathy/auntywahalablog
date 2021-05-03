8e416304092e681f22fd8d73df3229c59d96a518
Denise Onukwue <donukwue1@gmail.com>
2021-05-03 13:04:13 -0400

    adds navbar with dropdown links

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service';
+import React from "react";
+import { Link } from "react-router-dom";
+import * as userService from "../../utilities/users-service";
 
-export default function NavBar({ user, setUser }) {
-	function handleLogOut() {
-		// Delegate to the users-service
-		userService.logOut();
-		// Update state will also cause a re-render
-		setUser(null);
-	}
-
-	return (
-		<nav>
-			<Link to='/orders'>Order History</Link>
-			&nbsp; | &nbsp;
-			<Link to='/orders/new'>New Order</Link>
-			&nbsp; | &nbsp;
-			<span>{user.name}</span>
-			&nbsp; | &nbsp;
-			<Link to='' onClick={handleLogOut}>
-				Log Out
-			</Link>
-		</nav>
-	);
+export default function NavBar() {
+  return (
+    <>
+      <header>AUNTY WAHALA</header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <Link to="/" class="nav-item">
+              Home
+            </Link>
+            &nbsp; | &nbsp;
+            <li class="nav-item dropdown">
+              <Link
+                to="/"
+                class="nav-link dropdown-toggle"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Lifestyle
+              </Link>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <Link to="/real-life" class="dropdown-item">
+                  Real Life
+                </Link>
+                <div class="dropdown-divider"></div>
+                <Link to="/food" class="dropdown-item">
+                  Food
+                </Link>
+                <div class="dropdown-divider"></div>
+                <Link to="/side-eye-chronicles" class="dropdown-item">
+                  Side Eye Chronicles
+                </Link>
+                <div class="dropdown-divider"></div>
+                <Link to="/book-club" class="dropdown-item">
+                  Book Club
+                </Link>
+                <div class="dropdown-divider"></div>
+              </div>
+            </li>
+            &nbsp; | &nbsp;
+            <Link to="/school" class="nav-item">
+              School
+            </Link>
+            &nbsp; | &nbsp;
+            <Link to="/love" class="nav-item">
+              Love
+            </Link>
+            &nbsp; | &nbsp;
+            <Link to="/career" class="nav-item">
+              Career
+            </Link>
+            &nbsp; | &nbsp;
+            <Link to="/dear-aunty-wahala" class="nav-item">
+              Dear Aunty Wahala
+            </Link>
+            &nbsp; | &nbsp;
+            <Link to="/about-us" class="nav-item">
+              About Us
+            </Link>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -9,26 +9,8 @@ import NavBar from '../../Components/NavBar/NavBar';
 import './App.css';
 
 export default function App() {
-	const [user, setUser] = useState(getUser());
-
+	
 	return (
-		<main className='App'>
-			{user ? (
-				<>
-					<NavBar user={user} setUser={setUser} />
-					<Switch>
-						<Route path='/orders/new'>
-							<NewOrderPage />
-						</Route>
-						<Route path='/orders'>
-							<OrderHistoryPage />
-						</Route>
-						<Redirect to='/orders' />
-					</Switch>
-				</>
-			) : (
-				<AuthPage setUser={setUser} />
-			)}
-		</main>
+		<NavBar />
 	);
 }
